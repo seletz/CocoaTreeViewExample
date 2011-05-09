9c75b0271449f0f23e9b9d942110e2d606c2aa54
Stefan Eletzhofer <se@nexiles.de>
2011-05-09 11:40:09 +0200

    added treelistmodel stubs.

---
+++ Classes/TreeListModel.h
@@ -0,0 +1,16 @@
+//
+//  TreeListModel.h
+//  TreeList
+//
+//  Created by Stephan Eletzhofer on 09.05.11.
+//  Copyright 2011 Nexiles GmbH. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface TreeListModel : NSObject {
+
+}
+
+@end
---
+++ Classes/TreeListModel.m
@@ -0,0 +1,14 @@
+//
+//  TreeListModel.m
+//  TreeList
+//
+//  Created by Stephan Eletzhofer on 09.05.11.
+//  Copyright 2011 Nexiles GmbH. All rights reserved.
+//
+
+#import "TreeListModel.h"
+
+
+@implementation TreeListModel
+
+@end
